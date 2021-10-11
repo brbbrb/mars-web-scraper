2b03a5001b570643e1af3a3b44fb510918d3cb56
brbbrb <85380887+brbbrb@users.noreply.github.com>
2021-10-11 19:59:15 -0400

    stopping point

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,7 +1,23 @@
-
+# Import dependencies
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
+
+# Use flask_pymongo to set up connection to mongo
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def index():
+
+    # Find one record of data from the mongo database
+    mars_dict = mongo.db.mars_dict.find_one()
+    # Return template and data
+    return render_template("index.html", mars=mars_dict)
+
+## REWORD  THE ABOVE  ###
+####

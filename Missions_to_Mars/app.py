6b64f32923c5850d38820e7fcf39c198f3512aed
brbbrb <85380887+brbbrb@users.noreply.github.com>
2021-10-12 14:12:27 -0400

    flask app complete, added base html

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -6,18 +6,24 @@ import scrape_mars
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Use flask_pymongo to set up connection to mongo
+# Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-# Route to render index.html template using data from Mongo
+# Route to render index.html template with Mongo data
 @app.route("/")
 def index():
 
-    # Find one record of data from the mongo database
+    # Find one record of data from the mongo database and render
     mars_dict = mongo.db.mars_dict.find_one()
-    # Return template and data
-    return render_template("index.html", mars=mars_dict)
+    return render_template("index.html", mars_dict=mars_dict)
 
-## REWORD  THE ABOVE  ###
-####
+@app.route("/scrape")
+def scraper():
+    mars_dict = mongo.db.mars_dict
+    mars_data = scrape_mars.scrape()
+    mars_dict.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
